fb55bc715117c4ed930e31c66e9efb3b3e8d1a0f
vxtien <vxtien1012@gmail.com>
2023-03-22 11:19:53 +0700

    update

---
+++ 2/app.js
@@ -0,0 +1,11 @@
+let styles = ["Jazz", "Blues"];
+//2
+styles.push("Rock-n-Roll");
+//3
+styles.splice(1,1,"Classics")
+//4
+// let style4 = styles.shift();
+// console.log(style4);
+// //5
+// styles.unshift("Rap", "Reggae");
+console.log(styles);
