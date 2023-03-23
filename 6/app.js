6afdcc07443f2536b1f4b12c03dd3881a671b4a4
vxtien <vxtien1012@gmail.com>
2023-03-23 08:43:46 +0700

    update

--- 6/app.js
+++ 6/app.js
@@ -1,6 +1,6 @@
 function camelize(str) {
-  return str.split('-').map(function(word, text) {     
-    if (text === 0) {
+  return str.split('-').map(function(word, index) {     
+    if (index === 0) {
         return word;
     }
     return word.charAt(0).toUpperCase() + word.slice(1);
